433d4e57723915b0b3a0c24c049f68ab00067083
Claus <claus@claus.nu>
2020-05-20 10:23:28 +0200

    bumping version to 3.0.1.

--- src/Umbraco.Deploy.Contrib.Connectors/Properties/VersionInfo.cs
+++ src/Umbraco.Deploy.Contrib.Connectors/Properties/VersionInfo.cs
@@ -8,13 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
-using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyVersion("3.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: System.Reflection.AssemblyVersion("3.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.0.1")]
 
 
