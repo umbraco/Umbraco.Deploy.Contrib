bb99efbf723375d4c295c941612cfccb68e8dfe5
Andy Butland <abutland73@gmail.com>
2022-07-06 19:27:39 +0200

    Bumped version to 4.1.3.

--- src/Umbraco.Deploy.Contrib.Connectors/Properties/VersionInfo.cs
+++ src/Umbraco.Deploy.Contrib.Connectors/Properties/VersionInfo.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyInformationalVersion("4.1.2")]
-[assembly: System.Reflection.AssemblyVersion("4.1.2")]
+[assembly: System.Reflection.AssemblyInformationalVersion("4.1.3")]
+[assembly: System.Reflection.AssemblyVersion("4.1.3")]
 
 
