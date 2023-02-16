643c4bc87f2aaea92c3cfaa770cbdd3ea5e4b15b
Andy Butland <abutland73@gmail.com>
2023-02-16 10:12:02 +0100

    Bumped version to 4.2.1.

--- src/Umbraco.Deploy.Contrib.Connectors/Properties/AssemblyInfo.cs
+++ src/Umbraco.Deploy.Contrib.Connectors/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Umbraco.Deploy.Contrib")]
-[assembly: AssemblyCopyright("Copyright © Umbraco 2022")]
+[assembly: AssemblyCopyright("Copyright © Umbraco 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
