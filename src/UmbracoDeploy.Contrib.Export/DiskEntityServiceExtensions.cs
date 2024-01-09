ebfbb1337a6462a9932b539e45213a0cec5c552a
Ronald Barendse <ronald@barend.se>
2024-01-09 16:44:58 +0100

    Add UmbracoDeploy.Contrib.Export project

---
+++ src/UmbracoDeploy.Contrib.Export/DiskEntityServiceExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Umbraco.Core;
+using Umbraco.Core.Deploy;
+using Umbraco.Deploy;
+using Umbraco.Deploy.Disk;
+using Umbraco.Deploy.Files;
+
+namespace UmbracoDeploy.Contrib.Export
+{
+    internal static partial class DiskEntityServiceExtensions
+    {
+        public static IDisposable SuppressFileTypeCollection(this IDiskEntityService diskEntityService, out FileTypeCollection fileTypes)
+        {
+            var field = diskEntityService.GetType().GetField("_fileTypes", BindingFlags.Instance | BindingFlags.NonPublic);
+            var oldFileTypes = field.GetValue(diskEntityService) as FileTypeCollection;
+
+            // Replace with empty file type collection (so all artifacts are written as UDA files)
+            field.SetValue(diskEntityService, new FileTypeCollection(Enumerable.Empty<IFileType>()));
+            
+            fileTypes = oldFileTypes;
+
+            // Restore on dispose
+            return new InvokeOnDispose(() => field.SetValue(diskEntityService, oldFileTypes));
+        }
+
+        public static void WriteArtifacts(this IDiskEntityService diskEntityService, string path, IEnumerable<IArtifact> artifacts)
+        {
+            var field = diskEntityService.GetType().GetField("_afs", BindingFlags.Instance | BindingFlags.NonPublic);
+            var oldArtifactFileSystem = field.GetValue(diskEntityService);
+
+            // Replace artifact file system to use custom path to write artifacts to
+            field.SetValue(diskEntityService, new ArtifactFileSystem(path));
+
+            try
+            {
+                // Ensure directory exists
+                Directory.CreateDirectory(path);
+
+                diskEntityService.WriteArtifacts(artifacts);
+            }
+            finally
+            {
+                // Restore after artifacts are written
+                field.SetValue(diskEntityService, oldArtifactFileSystem);
+            }
+        }
+
+        public static void WriteFiles(this IDiskEntityService diskEntityService, string path, IEnumerable<IArtifact> artifacts, FileTypeCollection fileTypes)
+        {
+            foreach (var artifactByType in artifacts.GroupBy(x => x.Udi.EntityType))
+            {
+                if (!fileTypes.Contains(artifactByType.Key))
+                {
+                    continue;
+                }
+
+                var fileType = fileTypes[artifactByType.Key];
+                foreach (var udi in artifactByType.Select(x => x.Udi as StringUdi).WhereNotNull())
+                {
+                    // Ensure directory exists (since the path might not exist yet)
+                    var filePath = Path.Combine(path, artifactByType.Key, udi.Id);
+                    if (Path.GetDirectoryName(filePath) is string directoryPath)
+                    {
+                        Directory.CreateDirectory(directoryPath);
+                    }
+
+                    // Write file
+                    using (Stream stream = fileType.GetStream(udi))
+                    using (Stream destination = System.IO.File.OpenWrite(filePath))
+                    {
+                        stream.CopyTo(destination);
+                    }
+                }
+            }
+        }
+
+        private sealed class InvokeOnDispose : IDisposable
+        {
+            private readonly Action action;
+
+            public InvokeOnDispose(Action action) => this.action = action;
+
+            public void Dispose() => action.Invoke();
+        }
+    }
+}
---
+++ src/UmbracoDeploy.Contrib.Export/ServiceConnectorFactoryExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Deploy;
+using Umbraco.Deploy.ServiceConnectors;
+
+namespace UmbracoDeploy.Contrib.Export
+{
+    public static class ServiceConnectorFactoryExtensions
+    {
+        public static IEnumerable<IArtifact> GetArtifacts(this IServiceConnectorFactory serviceConnectorFactory, IEnumerable<Udi> udis)
+        {
+            foreach (var udiByType in udis.Distinct().GroupBy(x => x.EntityType))
+            {
+                if (serviceConnectorFactory.GetConnector(udiByType.Key) is IServiceConnector serviceConnector)
+                {
+                    foreach (var udi in udiByType)
+                    {
+                        if (serviceConnector.GetArtifact(udi) is IArtifact artifact)
+                        {
+                            yield return artifact;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<IArtifact> GetArtifacts(this IServiceConnectorFactory serviceConnectorFactory, IEnumerable<Udi> udis, string selector)
+        {
+            foreach (var udiByType in udis.Distinct().GroupBy(x => x.EntityType))
+            {
+                if (serviceConnectorFactory.GetConnector(udiByType.Key) is IServiceConnector serviceConnector)
+                {
+                    foreach (var udi in udiByType)
+                    {
+                        var namedUdiRange = serviceConnector.GetRange(udi, selector);
+
+                        foreach (var artifact in serviceConnector.GetArtifacts(namedUdiRange))
+                        {
+                            yield return artifact;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<IArtifact> GetArtifacts(this IServiceConnectorFactory serviceConnectorFactory, IEnumerable<Udi> udis, string[] dependencyEntityTypes)
+            => GetArtifactsRecursive(serviceConnectorFactory, GetArtifacts(serviceConnectorFactory, udis), dependencyEntityTypes);
+
+        public static IEnumerable<IArtifact> GetArtifacts(this IServiceConnectorFactory serviceConnectorFactory, IEnumerable<Udi> udis, string selector, string[] dependencyEntityTypes)
+            => GetArtifactsRecursive(serviceConnectorFactory, GetArtifacts(serviceConnectorFactory, udis, selector), dependencyEntityTypes);
+
+        private static IEnumerable<IArtifact> GetArtifactsRecursive(IServiceConnectorFactory serviceConnectorFactory, IEnumerable<IArtifact> artifacts, string[] dependencyEntityTypes)
+        {
+            var returnedUdis = new HashSet<Udi>();
+
+            foreach (IArtifact artifact in artifacts)
+            {
+                if (!returnedUdis.Add(artifact.Udi))
+                {
+                    // Already returned, so prevent recursive loop
+                    continue;
+                }
+
+                // Recurse artifact dependencies
+                foreach (var dependencyArtifact in GetArtifactsRecursive(serviceConnectorFactory, artifact.Dependencies, dependencyEntityTypes, returnedUdis))
+                {
+                    yield return dependencyArtifact;
+                }
+
+                yield return artifact;
+            }
+        }
+
+        private static IEnumerable<IArtifact> GetArtifactsRecursive(IServiceConnectorFactory serviceConnectorFactory, IEnumerable<ArtifactDependency> artifactDependencies, string[] entityTypes, ISet<Udi> returnedUdis)
+        {
+            // Only process specified entity types
+            var udis = artifactDependencies.Select(x => x.Udi).Where(x => entityTypes.Contains(x.EntityType));
+
+            foreach (var dependencyArtifact in GetArtifacts(serviceConnectorFactory, udis))
+            {
+                if (!returnedUdis.Add(dependencyArtifact.Udi))
+                {
+                    // Already returned, so prevent recursive loop
+                    continue;
+                }
+
+                // Recurse artifact dependencies
+                foreach (var recursiveDependencyArtifact in GetArtifactsRecursive(serviceConnectorFactory, dependencyArtifact.Dependencies, entityTypes, returnedUdis))
+                {
+                    yield return recursiveDependencyArtifact;
+                }
+
+                yield return dependencyArtifact;
+            }
+        }
+    }
+}
