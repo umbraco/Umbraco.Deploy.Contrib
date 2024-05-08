b87019cb8c73a3bc1827a7ca28442de6e29403cf
Andy Butland <abutland73@gmail.com>
2024-05-08 07:23:18 +0200

    Update to CMS and Deploy 14 and version to 14.0.0-beta001. Comment out migrators that need amending due to breaking changes or conversion to System.Text.Json.

--- src/Umbraco.Deploy.Contrib/Migrators/Legacy/ContentType/ContentTypeArtifactJsonMigrator.cs
+++ src/Umbraco.Deploy.Contrib/Migrators/Legacy/ContentType/ContentTypeArtifactJsonMigrator.cs
@@ -1,44 +1,44 @@
-using System;
-using Newtonsoft.Json.Linq;
-using Umbraco.Cms.Core.Semver;
-using Umbraco.Deploy.Infrastructure.Artifacts.ContentType;
-using Umbraco.Deploy.Infrastructure.Migrators;
-using Umbraco.Extensions;
+//using System;
+//using Newtonsoft.Json.Linq;
+//using Umbraco.Cms.Core.Semver;
+//using Umbraco.Deploy.Infrastructure.Artifacts.ContentType;
+//using Umbraco.Deploy.Infrastructure.Migrators;
+//using Umbraco.Extensions;
 
-namespace Umbraco.Deploy.Contrib.Migrators.Legacy;
+//namespace Umbraco.Deploy.Contrib.Migrators.Legacy;
 
-/// <summary>
-/// Migrates the <see cref="ContentTypeArtifactBase" /> JSON from Umbraco 7 allowed at root and child content types to permissions.
-/// </summary>
-public class ContentTypeArtifactJsonMigrator : ArtifactJsonMigratorBase<ContentTypeArtifactBase>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ContentTypeArtifactJsonMigrator" /> class.
-    /// </summary>
-    public ContentTypeArtifactJsonMigrator()
-        => MaxVersion = new SemVersion(3, 0, 0);
+///// <summary>
+///// Migrates the <see cref="ContentTypeArtifactBase" /> JSON from Umbraco 7 allowed at root and child content types to permissions.
+///// </summary>
+//public class ContentTypeArtifactJsonMigrator : ArtifactJsonMigratorBase<ContentTypeArtifactBase>
+//{
+//    /// <summary>
+//    /// Initializes a new instance of the <see cref="ContentTypeArtifactJsonMigrator" /> class.
+//    /// </summary>
+//    public ContentTypeArtifactJsonMigrator()
+//        => MaxVersion = new SemVersion(3, 0, 0);
 
-    protected override bool CanMigrateType(Type type)
-        => type.Inherits<ContentTypeArtifactBase>();
+//    protected override bool CanMigrateType(Type type)
+//        => type.Inherits<ContentTypeArtifactBase>();
 
-    /// <inheritdoc />
-    public override JToken Migrate(JToken artifactJson)
-    {
-        var permissions = new JObject();
+//    /// <inheritdoc />
+//    public override JToken Migrate(JToken artifactJson)
+//    {
+//        var permissions = new JObject();
 
-        if (artifactJson["AllowedAtRoot"] is JValue allowedAtRootValue &&
-            allowedAtRootValue.Value is not null)
-        {
-            permissions["AllowedAtRoot"] = allowedAtRootValue;
-        }
+//        if (artifactJson["AllowedAtRoot"] is JValue allowedAtRootValue &&
+//            allowedAtRootValue.Value is not null)
+//        {
+//            permissions["AllowedAtRoot"] = allowedAtRootValue;
+//        }
 
-        if (artifactJson["AllowedChildContentTypes"] is JArray allowedChildContentTypesToken)
-        {
-            permissions["AllowedChildContentTypes"] = allowedChildContentTypesToken;
-        }
+//        if (artifactJson["AllowedChildContentTypes"] is JArray allowedChildContentTypesToken)
+//        {
+//            permissions["AllowedChildContentTypes"] = allowedChildContentTypesToken;
+//        }
 
-        artifactJson["Permissions"] = permissions;
+//        artifactJson["Permissions"] = permissions;
 
-        return artifactJson;
-    }
-}
+//        return artifactJson;
+//    }
+//}
--- src/Umbraco.Deploy.Contrib/Migrators/Legacy/DataType/PreValuesDataTypeArtifactJsonMigrator.cs
+++ src/Umbraco.Deploy.Contrib/Migrators/Legacy/DataType/PreValuesDataTypeArtifactJsonMigrator.cs
@@ -1,47 +1,47 @@
-using Newtonsoft.Json.Linq;
-using Umbraco.Cms.Core.Semver;
-using Umbraco.Deploy.Infrastructure.Artifacts;
-using Umbraco.Deploy.Infrastructure.Migrators;
-using Umbraco.Extensions;
+//using Newtonsoft.Json.Linq;
+//using Umbraco.Cms.Core.Semver;
+//using Umbraco.Deploy.Infrastructure.Artifacts;
+//using Umbraco.Deploy.Infrastructure.Migrators;
+//using Umbraco.Extensions;
 
-namespace Umbraco.Deploy.Contrib.Migrators.Legacy;
+//namespace Umbraco.Deploy.Contrib.Migrators.Legacy;
 
-/// <summary>
-/// Migrates the <see cref="DataTypeArtifact" /> JSON from Umbraco 7 pre-values to configuration objects/arrays.
-/// </summary>
-public class PreValuesDataTypeArtifactJsonMigrator : ArtifactJsonMigratorBase<DataTypeArtifact>
-{
-    /// <summary>
-    /// Initializes a new instance of the <see cref="PreValuesDataTypeArtifactJsonMigrator" /> class.
-    /// </summary>
-    public PreValuesDataTypeArtifactJsonMigrator()
-      => MaxVersion = new SemVersion(3, 0, 0);
+///// <summary>
+///// Migrates the <see cref="DataTypeArtifact" /> JSON from Umbraco 7 pre-values to configuration objects/arrays.
+///// </summary>
+//public class PreValuesDataTypeArtifactJsonMigrator : ArtifactJsonMigratorBase<DataTypeArtifact>
+//{
+//    /// <summary>
+//    /// Initializes a new instance of the <see cref="PreValuesDataTypeArtifactJsonMigrator" /> class.
+//    /// </summary>
+//    public PreValuesDataTypeArtifactJsonMigrator()
+//      => MaxVersion = new SemVersion(3, 0, 0);
 
-    /// <inheritdoc />
-    public override JToken Migrate(JToken artifactJson)
-    {
-        if (artifactJson["PreValues"] is JObject preValues)
-        {
-            var configuration = new JObject();
+//    /// <inheritdoc />
+//    public override JToken Migrate(JToken artifactJson)
+//    {
+//        if (artifactJson["PreValues"] is JObject preValues)
+//        {
+//            var configuration = new JObject();
 
-            foreach (var property in preValues.Properties())
-            {
-                var propertyValue = property.Value;
+//            foreach (var property in preValues.Properties())
+//            {
+//                var propertyValue = property.Value;
 
-                // Convert pre-value serialized JSON to actual JSON objects/arrays
-                if (propertyValue.Type == JTokenType.String &&
-                    propertyValue.Value<string>() is string json &&
-                    json.DetectIsJson())
-                {
-                    propertyValue = JToken.Parse(json);
-                }
+//                // Convert pre-value serialized JSON to actual JSON objects/arrays
+//                if (propertyValue.Type == JTokenType.String &&
+//                    propertyValue.Value<string>() is string json &&
+//                    json.DetectIsJson())
+//                {
+//                    propertyValue = JToken.Parse(json);
+//                }
 
-                configuration.Add(property.Name, propertyValue);
-            }
+//                configuration.Add(property.Name, propertyValue);
+//            }
 
-            artifactJson["Configuration"] = configuration;
-        }
+//            artifactJson["Configuration"] = configuration;
+//        }
 
-        return artifactJson;
-    }
-}
+//        return artifactJson;
+//    }
+//}
